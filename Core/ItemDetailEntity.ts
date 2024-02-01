70ee21be0e4b2929fba88198766e49760aea7453
Freddy Lezama <freddy.lezama@possumus.tech>
2024-02-01 11:12:00 -0300

    elimino archivo no usado

--- Core/ItemDetailEntity.ts
+++
@@ -1,9 +0,0 @@
-export interface IPeopleDetailEntity {
-    name: string,
-    gender: string,
-    mass: string,
-    height: string,
-    birthYear: string,
-    hairColor: string,
-    eyesColor: string
-}
